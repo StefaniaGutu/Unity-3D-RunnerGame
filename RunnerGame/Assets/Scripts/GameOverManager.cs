797e0eebeeacc0a9791a7fa63c34f0772bb1c85b
mihaivladgeorge <vladgeorgemihai2001@gmail.com>
2023-01-05 01:32:01 +0200

    Added start game scene

--- RunnerGame/Assets/Scripts/GameOverManager.cs
+++ RunnerGame/Assets/Scripts/GameOverManager.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameOverManager : MonoBehaviour
 {
     [SerializeField] GameObject gameOverScreen;
 
+    [SerializeField] PlayerController playerController;
+
     public void SetGameOver()
     {
         gameOverScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        //playerController.setSpeedEnd();
+        SceneManager.LoadScene(1);
     }
 }
